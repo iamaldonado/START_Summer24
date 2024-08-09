c35e6a1271a5368705a5f70b6990c5e8c0f7d1e7
JCMarqzRa <juan.marquezra@gmail.com>
2024-08-09 15:05:06 +0300

    Correción a este macro para correr muchos eventos. PS. No se corrio aquí ya que fue demasiada información y se llenaba. Por lo que se decidio correr todo en el ~/scratch2/marquez. Más adelante mandaré correcciones para que sea más sencillo.

--- CarlosMarquez/lowMgF/macros/manyjobs.sh
+++ CarlosMarquez/lowMgF/macros/manyjobs.sh
@@ -5,17 +5,41 @@ do
 
 mkdir dir${INDEX}
 
-cp runanauno dir${INDEX}/runanauno
-cp NucleiAna.json dir${INDEX}/NucleiAna.json
-cp pCentr.txt dir${INDEX}/pCentr.txt
 cp listareq28_${INDEX}.txt dir${INDEX} 
 
-sed -e "s/listTEST/listareq28_${INDEX}/" RunAnalyses.C > dir${INDEX}/RunAnalyses.C
-
 cd dir${INDEX}
-sbatch runanauno
+
+split -l 556 -d listareq28_${INDEX}.txt
+
+mkdir xa0 
+
+mkdir xa1
+
+cp x00 xa0/x00
+cp x01 xa1/x01
+
 cd ..
 
+cp runanauno dir${INDEX}/xa0/runanauno
+cp NucleiAna.json dir${INDEX}/xa0/NucleiAna.json
+cp pCentr.txt dir${INDEX}/xa0/pCentr.txt
+
+cp runanauno dir${INDEX}/xa1/runanauno
+cp NucleiAna.json dir${INDEX}/xa1/NucleiAna.json
+cp pCentr.txt dir${INDEX}/xa1/pCentr.txt
+
+sed -e "s/listTEST/x00/" RunAnalyses.C > dir${INDEX}/xa0/RunAnalyses.C
+
+sed -e "s/listTEST/x01/" RunAnalyses.C > dir${INDEX}/xa1/RunAnalyses.C
+ 
+cd dir${INDEX}/xa0/
+
+sbatch runanauno
+
+cd ../xa1
+
+sbatch runanauno
+
+cd ../..
 
 done
-
