5dd9bd42af8db283a1cdabc9b9a4b222e40dd9d8
JCMarqzRa <juan.marquezra@gmail.com>
2024-08-21 15:16:07 +0300

    Ordenar h.

--- CarlosMarquez/lowMgF/lowMgF.h
+++ CarlosMarquez/lowMgF/lowMgF.h
@@ -31,12 +31,12 @@ public:
   
 //__________________________________________________________________________________
 
-  TClonesArray *mMCTracks                       = nullptr;
-  TClonesArray *mKalmanTracks                   = nullptr;
-  TClonesArray *mMpdGlobalTracks                = nullptr;
-  TClonesArray *tofMatches                      = nullptr;
-  TClonesArray *vtxs 			        = nullptr;
-  FairMCEventHeader *mMCEventHeader 		= nullptr;
+  TClonesArray *mMCTracks                       = nullptr;	// Monte-Carlo tracks
+  TClonesArray *mKalmanTracks                   = nullptr;	// TPC Kalman tracks
+  TClonesArray *mMpdGlobalTracks                = nullptr;	// MPD Global tracks
+  TClonesArray *tofMatches                      = nullptr;	// ToF Matching
+  TClonesArray *vtxs 			        = nullptr;	// Vertex Position
+  FairMCEventHeader *mMCEventHeader 		= nullptr;	// MC Event Header track
 
 //__________________________________________________________________________________
 
@@ -57,65 +57,82 @@ public:
    TProfile2D *TPDZNtracksW;	// TProfile2D/Weight Resolution vs Number of Track
    TProfile2D *TPDZbW;		// TProfile2D/Weight Resolution vs Impact Parameter
 
+   TProfile *VtxMult;		// Transvese Momentum vs Multiplicity
+
 //-------Tranverse Momemtum----------
 
-   TH2F *PtMCvsEta;		// Monte Carlos versus Psuedo-Rapidity
+   TH2F *PtMCvsEta;		// Monte-Carlos vs Psuedo-Rapidity
 
-   TH2F *PtRECOvsEta;		// Recontruction versus Pseudo-Rapidity
+   TH2F *PtRECOvsEta;		// Recontructed vs Pseudo-Rapidity
+
+   TProfile *DPtPtReco;		// Transverse Momentum as a function of Transverse Momentum
 
 	// without Cuts
    TProfile *PtNHits;		// Number of Hits
+   TProfile *PtEta;		// Pseudo-Rapidity
    TProfile *PtDCAGlobal;	// DCA Global
    TProfile *PtDCAGlobalP;	// DCA Global Primary
    TProfile *PtDCAGlobalS;	// DCA Global Secondary
-   TProfile *PtEta;		// PseudoRapidity
 
-   TProfile2D *PtEtaDPt;     	// Resolution
+   TProfile2D *PtEtaDPt;     	// Resolution of the transverse momentum
 
 	// With Cuts in NHits
    TProfile *PtNHitsC;		// Number of Hits
+   TProfile *PtEtaC;		// PseudoRapidity 
    TProfile *PtDCAGlobalC;	// DCA Global
    TProfile *PtDCAGlobalPC;	// DCA Global Primary
    TProfile *PtDCAGlobalSC;	// DCA Global Secondary
-   TProfile *PtEtaC;		// PseudoRapidity 
 
    TProfile2D *PtEtaDPtC; 	// Resolution
 
 	// With Cuts in Eta
    TProfile *PtNHitsCE;		// Number of Hits
+   TProfile *PtEtaCE;		// PseudoRapidity 
    TProfile *PtDCAGlobalCE;	// DCA Global
    TProfile *PtDCAGlobalPCE;	// DCA Global Primary
    TProfile *PtDCAGlobalSCE;	// DCA Global Secondary
-   TProfile *PtEtaCE;		// PseudoRapidity 
 
    TProfile2D *PtEtaDPtCE; 	// Resolution
 
+	// With Cuts in DCA Global
+   TProfile *PtNHitsDCA;	// Number of Hits
+   TProfile *PtEtaDCA;		// PseudoRapidity 
+   TProfile *PtDCAGlobalDCA;	// DCA Global
+   TProfile *PtDCAGlobalPDCA;	// DCA Global Primary
+   TProfile *PtDCAGlobalSDCA;	// DCA Global Secondary
 
-	// DCA Global with cuts in Number of Hits
+   TProfile2D *PtEtaDPtDCA; 	// Resolution
+
+	// With Cuts in Transverse Momentum
+   TProfile *PtNHitsCPT;	// Number of Hits
+   TProfile *PtEtaCPT;		// PseudoRapidity 
+   TProfile *PtDCAGlobalCPT;	// DCA Global
+   TProfile *PtDCAGlobalPCPT;	// DCA Global Primary
+   TProfile *PtDCAGlobalSCPT;	// DCA Global Secondary
+
+   TProfile2D *PtEtaDPtCPT; 	// Resolution
+
+//-------DCA Global with cuts in Number of Hits
+   TH1F *DCAGC;			// DCA Global
    TH1F *DCAGPC; 		// DCA Global Primary
    TH1F *DCAGSC; 		// DCA Global Secondary
-   TH1F *DCAGC;			// DCA Global
 
-	// Track Efficiency
-   TH1F *PtRecoPionP;
-   TH1F *PtRecoPionS;
-   TH1F *PtMCPionP;
-   TH1F *PtMCPionS;
+//-------Track Efficiency-----------------------
 
-   TH1F *PtRecoProtonP;
-   TH1F *PtRecoProtonS;
-   TH1F *PtMCProtonP;
-   TH1F *PtMCProtonS;
+   TH1F *PtRecoPionP;		// Reconstruted Primary Pions
+   TH1F *PtRecoPionS;		// Reconstruted Secondary Pions
+   TH1F *PtMCPionP;		// Monte-Carlo Primary Pions
+   TH1F *PtMCPionS;		// Monte-Carlo Secondary Pions
 
-   TH1F *PtRecoKaonP;
-   TH1F *PtRecoKaonS;
-   TH1F *PtMCKaonP;
-   TH1F *PtMCKaonS;
+   TH1F *PtRecoProtonP;		// Reconstruted Primary Protons
+   TH1F *PtRecoProtonS;		// Reconstruted Secondary Protons
+   TH1F *PtMCProtonP;		// Monte-Carlo Primary Protons
+   TH1F *PtMCProtonS;		// Monte-Carlo Secondary Protons
 
-   TProfile *DPtPtReco;
-
-	// Multiplicity
-   TProfile *VtxMult;
+   TH1F *PtRecoKaonP;		// Reconstruted Primary Kaons
+   TH1F *PtRecoKaonS;		// Reconstruted Secondary Kaons
+   TH1F *PtMCKaonP;		// Monte-Carlo Primary Kaons
+   TH1F *PtMCKaonS;		// Monte-Carlo Secondary Kaons
 
   ClassDef(lowMgF,1);
 };
